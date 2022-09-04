195d1fa02ebc63462102725eaa370a195669807f
jonghoon_lee <adjh54ir@gmail.com>
2022-09-04 23:05:22 +0900

    1day 1commit

--- pages/common/Header.tsx
+++ pages/common/Header.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 const Header = () => {
-
 	const headerContent =
 		'block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700';
 
@@ -67,7 +66,6 @@ const Header = () => {
 								</Link>
 							</li>
 							<li>
-<<<<<<< HEAD
 								<Link href='/lunch/main'>
 									<a href='#' className={headerContent}>
 										점심메뉴
@@ -76,8 +74,6 @@ const Header = () => {
 							</li>
 
 							<li>
-=======
->>>>>>> 2a73bea567495acb9bfe04b850f9857e78f01041
 								<Link href={`/blog/Scope`}>
 									<a href='#' className={headerContent}>
 										Scope
