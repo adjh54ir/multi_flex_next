942ca4935d944a7363698ab6a41ca2b64bfe68d3
jonghoon_lee <adjh54ir@gmail.com>
2022-10-10 23:35:32 +0900

    no message

--- pages/blog/EventDelgation.tsx
+++ pages/blog/EventDelgation.tsx
@@ -1,44 +1,29 @@
-import { LegacyRef, MutableRefObject, useEffect, useRef } from "react"
+import { useState } from 'react';
 
 const EventDelgation = () => {
+	const [btnIdx, setBtnIdx] = useState(0);
 
-    const menuRef = useRef<HTMLDivElement>(null);
+	const onclickMenu = (e) => {
+		if (e.currentTarget) {
+			const flag = e.currentTarget.children[btnIdx].getAttribute('data-action');
+			if (flag === 'save') alert('저장하기');
+			if (flag === 'edit') alert('수정하기');
+			if (flag === 'delete') alert('삭제하기');
+		}
+	};
 
-    useEffect(() => {
-        // 
-        // onclickMenu()
-    }, []);
-
-    const onclickMenu = (e) => {
-        console.log("onclick Menu")
-
-        // console.log(e.target.attributes.getNamedItem("data-action").value);
-
-        const eventAction = e.target.dataset.action;
-        if (eventAction) progressMenu[eventAction]();
-
-    }
-
-
-    const progressMenu = () => {
-        return {
-            save: () => alert("저장하기 "),
-            edit: () => alert("수정하기 "),
-            delelte: () => alert("삭제하기 ")
-        }
-    }
-
-
-
-
-
-    return (
-        <div id="menu" ref={menuRef} data-action="data-save" style={{ backgroundColor: "red" }} onClickCapture={(e) => onclickMenu(e)}>
-            <button id="save" type="button" data-action="data-save">저장하기 </button>
-            <button id="edit" type="button" data-action="data-edit">수정하기 </button>
-            <button id="delete" type="button" data-action="data-delete">삭제하기  </button>
-        </div>
-    )
-
-}
-export default EventDelgation
+	return (
+		<div id='menu' style={{ backgroundColor: 'red' }} onClick={(e) => onclickMenu(e)}>
+			<button id='save' type='button' data-action='save_0' onMouseUp={() => setBtnIdx(0)}>
+				저장하기
+			</button>
+			<button id='edit' type='button' data-action='edit_1' onMouseUp={() => setBtnIdx(1)}>
+				수정하기
+			</button>
+			<button id='delete' type='button' data-action='delete_2' onMouseUp={() => setBtnIdx(2)}>
+				삭제하기
+			</button>
+		</div>
+	);
+};
+export default EventDelgation;
--- pages/test/testArea.tsx
+++ pages/test/testArea.tsx
@@ -7,7 +7,26 @@ const testArea = () => {
 	}, []);
 
 	const mainTest = () => {
-		//
+		/**
+		 * str.toUpperCase()의 속성
+		 * @return {string} : 대문자로 변환한 새로운 문자열입니다.
+		 */
+		// str.toUpperCase()
+
+		/**
+		 * str.toLowerCase()의 속성
+		 * @return {string} :소문자로 변환된 호출 문자열을 나타내는 새 문자열입니다.
+		 */
+		// str.toLowerCase()
+
+		/**
+		 * 기본 사용 방법
+		 */
+		const lowerStr = 'hello';
+		lowerStr.toUpperCase(); // "HELLO"
+
+		const upperStr = 'HELLO';
+		upperStr.toLowerCase(); // hello
 	};
 
 	return (
